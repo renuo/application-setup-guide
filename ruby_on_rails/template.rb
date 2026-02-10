e3d4b728998996a4c4c3326b83d9ecac857bc314
coorasse <coorasse@users.noreply.github.com>
2026-02-10 09:53:10 +0000

    deploy: 0177ba72a64333eddc73bb349bbf4c28ae9cf9da

--- ruby_on_rails/template.rb
+++ ruby_on_rails/template.rb
@@ -8,6 +8,12 @@ insert_into_file "Gemfile", after: /^group :development do\n/ do
   RUBY
 end
 
+insert_into_file "Gemfile", after: /^group :development, :test do\n/ do
+  <<~RUBY
+    gem "dotenv"
+  RUBY
+end
+
 # Optimize Propshaft asset event-loop on macOS, see https://github.com/rails/propshaft/tree/main?tab=readme-ov-file#improving-performance-in-development
 gem_group :development, :test do
   gem "listen"
@@ -22,7 +28,7 @@ gsub_file "bin/setup", "bin/rails db:prepare", "bin/rails db:setup"
 insert_into_file "bin/setup", before: "\n  puts \"\\n== Preparing database ==\"" do
   <<-RUBY
   puts "\\n== Fetching 1password dependencies =="
-  system! 'renuo fetch-secrets'  
+  system! 'renuo fetch-secrets'
   RUBY
 end
 
@@ -42,6 +48,45 @@ create_file ".rubocop.yml", force: true do
     inherit_gem:
       renuocop: config/base.yml
   RUBOCOP
+end
+
+answer = ask("Do you want to setup dotenv? (y/n)", default: "y")
+
+if answer.downcase == "y"
+  insert_into_file "Gemfile", after: /^group :development, :test do\n/ do
+    <<~RUBY
+    gem "dotenv"
+  RUBY
+  end
+
+  create_file ".env.example", force: true do
+    <<~ENV
+      SECRET_KEY_BASE=<%= `bin/rails secret`.strip %>
+      APP_PORT=3000
+    ENV
+  end
+
+  create_file ".env", force: true do
+    <<~ENV
+      SECRET_KEY_BASE=#{SecureRandom.hex(64)}
+      APP_PORT=3000
+    ENV
+  end
+
+  create_file "config/initializers/dotenv.rb", force: true do
+    <<~DOTENV
+      Dotenv.require_keys(Dotenv.parse(".env.example").keys)
+    DOTENV
+  end
+
+  insert_into_file "bin/setup", before: "\n  puts \"\\n== Preparing database ==\"" do
+    <<-RUBY
+    puts "\n== Copying sample files =="
+    unless File.exist?('.env')
+      system! 'cp .env.example .env'
+    end
+    RUBY
+  end
 end
 
 # convenience script to run the app locally (however the developer intended, probably similar to bin/dev nowadays)

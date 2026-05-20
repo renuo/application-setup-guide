79e64f6917ecf529a3a55bac56ca242a04f3fe93
lukasbischof <lukasbischof@users.noreply.github.com>
2026-05-20 13:06:19 +0000

    deploy: aff5f0ecca0f17bcaa9f1ac22b4126b1d3190926

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dc5d1b66.js',
+                path_to_root + 'searchindex-35ba97ec.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

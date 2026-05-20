0462d47c02e528abe763b1717d71178511d983af
Dani Bengl <daniel.bengl@renuo.ch>
2026-05-20 11:18:24 +0200

    Revert "Cucumber suggestion"
    This reverts commit e099a8c9b7e185d0fc93f28efc677a89647f6db6.

---
+++ templates/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  def check
+    val = ActiveRecord::Base.connection.execute('select 1+2 as val').first['val']
+    render plain: "1+2=#{val}"
+  end
+end

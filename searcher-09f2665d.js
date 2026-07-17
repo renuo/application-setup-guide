288b58d3cec04ae2ba1b2e45ca8d6ed404cebd82
cb341 <cb341@users.noreply.github.com>
2026-07-17 08:46:46 +0000

    deploy: a579594ff44ac3c184113ce580ccd1c1c8464985

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5ab0665e.js',
+                path_to_root + 'searchindex-57445ef8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
